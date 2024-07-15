ef116fe71d877182d09b8f4901313e7039b6b484
r3quie <r3quie@gmail.com>
2024-07-15 22:10:08 +0200

    works:)

--- main.go
+++ main.go
@@ -9,38 +9,51 @@ import (
 )
 
 func main() {
+	date := inputtime("Enter date in format 12.4.2024: \n")
+	var delta int
+	for {
+		print("Enter number of days: ")
+		_, err := fmt.Scan(&delta)
+		//print(delta, "je delta\n", err, "je err")
+		if err != nil {
+			fmt.Println("Invalid input")
+			continue
+		}
+		break
+	}
+	print(doruceni(date, delta))
+}
+
+func doruceni(inputdate time.Time, inputdelta int) string {
 	c := cal.NewBusinessCalendar()
 	c.AddHoliday(cz.Holidays...)
-	t := inputdate("Enter date in format 12.4.2024: \n")
-	var delta int = input("Enter number of days to add: ", int)
+	konec := inputdate.AddDate(0, 0, inputdelta)
+	var before_konec time.Time = konec
+	posunuto := false
+	for !c.IsWorkday(konec) {
+		posunuto = true
+		konec = konec.AddDate(0, 0, 1)
+	}
+	if !posunuto {
+		return f("Konec lhuty bude %d.%d.%d", konec.Day(), konec.Month(), konec.Year())
+	}
+	return f("Konec lhuty měl být %d.%d.%d, ale kvuli svátku nebo víkendu bude až %d.%d.%d", before_konec.Day(), before_konec.Month(), before_konec.Year(), konec.Day(), konec.Month(), konec.Year())
 }
 
-func doruceni() []*cal.Holiday {
-	holidays := cz.Holidays
-	return holidays
-}
-
-func inputdate(prompt string) time.Time {
+func inputtime(prompt string) time.Time {
 	var d, m, y int
 	fmt.Print(prompt)
 	fmt.Scanf("%d.%d.%d", &d, &m, &y)
 	return time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.UTC)
 }
 
-func input(prompt string) any {
-	var input any
-	fmt.Print(prompt)
-	fmt.Scan(&input)
-	return input
-}
-
 func print(input ...any) {
 	fmt.Println(input...)
 }
 
-func printf(inputstr string, input ...any) {
+/* func printf(inputstr string, input ...any) {
 	fmt.Printf(inputstr+"\n", input...)
-}
+} */
 
 func f(inputstr string, input ...any) string {
 	return fmt.Sprintf(inputstr, input...)
