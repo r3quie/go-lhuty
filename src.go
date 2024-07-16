58aaf47319c2cdbc17dcadc71be5a2b678a7e11a
r3quie <r3quie@gmail.com>
2024-07-16 14:20:03 +0200

    some pretty logic

--- src.go
+++ src.go
@@ -20,19 +20,38 @@ func doruceni(date time.Time, delta int) string {
 	konec := date.AddDate(0, 0, delta)
 	var before_konec time.Time = konec
 	posunuto := false
+	svatek := false
+	vikend := false
+	svatek_or_vikend := ""
 	for !c.IsWorkday(konec) {
 		posunuto = true
+		h, _, n := c.IsHoliday(konec)
+		if h && svatek {
+			svatek_or_vikend += f("a svátku '%s' ", n.Name)
+		} else if h {
+			svatek = true
+			svatek_or_vikend += f("svátku '%s' ", n.Name)
+		}
+		if konec.Weekday() == 0 || konec.Weekday() == 6 {
+			vikend = true
+		}
 		konec = konec.AddDate(0, 0, 1)
 	}
 	if !posunuto {
-		return f("Konec lhuty bude %d.%d.%d (%s)", konec.Day(), konec.Month(), konec.Year(), convert_weekday(konec.Weekday()))
+		return f("Konec lhůty bude %d.%d.%d (%s)", konec.Day(), konec.Month(), konec.Year(), convert_weekday(konec.Weekday()))
 	}
 	weekday_before_konec := convert_weekday(before_konec.Weekday())
 	is_holiday, _, name_of_holiday := c.IsHoliday(before_konec)
 	if is_holiday {
 		weekday_before_konec += f(", %s", name_of_holiday.Name)
 	}
-	return f("Konec lhuty měl být %d.%d.%d (%s), ale kvuli svátku nebo víkendu bude až %d.%d.%d (%s)", before_konec.Day(), before_konec.Month(), before_konec.Year(), weekday_before_konec, konec.Day(), konec.Month(), konec.Year(), convert_weekday(konec.Weekday()))
+	if !svatek {
+		svatek_or_vikend = "víkendu"
+	}
+	if svatek && vikend {
+		svatek_or_vikend += "a víkendu"
+	}
+	return f("Konec lhůty měl být %d.%d.%d (%s), ale kvůli %s bude až \n%d.%d.%d (%s)", before_konec.Day(), before_konec.Month(), before_konec.Year(), weekday_before_konec, svatek_or_vikend, konec.Day(), konec.Month(), konec.Year(), convert_weekday(konec.Weekday()))
 }
 
 /*func input_time(prompt string) time.Time {
