490b6903836b7f3d373c0774ea9098d2fa3d337a
r3quie <r3quie@gmail.com>
2024-07-16 16:26:56 +0200

    observer pain...

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -13,16 +14,25 @@ func main() {
 	w := a.NewWindow("Kalkulačka lhůt")
 	dnes := time.Now()
 
+	zacatekstr := binding.NewString()
+	zacatekstr.Set(f("%d.%d.%d", dnes.Day(), dnes.Month(), dnes.Year()))
+	dnustr := binding.NewString()
+	dnustr.Set("8")
+	vysledekstr := binding.NewString()
+
 	title := widget.NewLabel("Kalkulačka lhůt")
 	test := widget.NewLabel("Zadejte datum a počet dní")
 
-	zacatek := widget.NewEntry()
-	zacatek.SetPlaceHolder(f("%d.%d.%d", dnes.Day(), dnes.Month(), dnes.Year()))
+	zacatek := widget.NewEntryWithData(zacatekstr)
 
-	dnu := widget.NewEntry()
-	dnu.SetPlaceHolder("8")
+	dnu := widget.NewEntryWithData(dnustr)
 
-	vysledek := widget.NewLabel("")
+	workv, _ := zacatekstr.Get()
+	workdnu, _ := dnustr.Get()
+
+	vysledekstr.Set(doruceni(string_to_time(workv), string_to_int(workdnu)))
+
+	vysledek := widget.NewLabelWithData(vysledekstr)
 
 	w.SetContent(container.NewVBox(
 		title,
@@ -30,9 +40,9 @@ func main() {
 		zacatek,
 		dnu,
 		vysledek,
-		widget.NewButton("Spočítat", func() {
+		/*widget.NewButton("Spočítat", func() {
 			vysledek.SetText(doruceni(string_to_time(zacatek.Text), string_to_int(dnu.Text)))
-		}),
+		}*/
 	))
 
 	w.ShowAndRun()
